9c259e15d00b38117c96f53939f67923f487d83a
paolo_home <paolalessio.05.p@gmail.com>
2026-05-25 22:10:07 +0200

    adding init.lua

--- lua/dnd_generator/init.lua
+++ lua/dnd_generator/init.lua
@@ -1,13 +1,20 @@
 local M = {}
 
 M.config = {
-
+  mode = "offline",
+  cache_dir = vim.fn.stdpath("data") .. "/dnd_generator"
 }
 
 M.setup = function (opts)
   opts = opts or {}
 
   M.config = vim.tbl_deep_extend("force", M.config, opts)
+
+  if M.config.mode == "offline" then
+    if vim.fn.isdirectory(M.config.cache_dir) == 0 then
+      vim.fn.mkdir(M.config.cache_dir, "p")
+    end
+  end
 end
 
 
